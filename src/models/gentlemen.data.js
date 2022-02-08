78b116fcd7615f960e5b8822b442a2142599a9c2
Mel Escolano <amalia.escolano@gmail.com>
2022-02-08 21:08:40 +0100

    Create components and set functions to buttons

---
+++ src/models/gentlemen.data.js
@@ -0,0 +1,32 @@
+export const GENTLEMEN = [
+  {
+    id: 1,
+    name: 'Bertin Osborne',
+    status: 'Alive',
+    profession: 'Youtuber',
+    twitter: '@osbourne',
+    picture: 'bertin.jpg',
+    alternativeText: 'Osbourne pointing at you',
+    selected: true,
+  },
+  {
+    name: 'The Farytale',
+    status: 'RIP',
+    profession: 'Influencer',
+    twitter: 'pending',
+    picture: 'fary.jpg',
+    alternativeText: 'The Fary pointing at you',
+    id: 2,
+    selected: false,
+  },
+  {
+    id: 3,
+    name: 'Julius Churchs',
+    status: 'Alive',
+    profession: 'Java developer',
+    twitter: '@julius_churchs',
+    picture: 'julio.jpg',
+    alternativeText: 'Churchs pointing at you',
+    selected: true,
+  },
+];
